1595c1d6096e80b113c34fe7efbb7ad747c04676
Joaquin Nahuel Giordano Vajda <joao@Joaquins-MacBook-Pro.local>
2026-06-22 19:53:54 -0300

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
+// vite.config.ts
 export default defineConfig({
+  base: '/', // si el repo se llama originalharu.github.io
+  // o
+  base: '/nombre-del-repo/', // si es un repo cualquiera
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+})
